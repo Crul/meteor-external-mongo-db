9a30f5452cc5bded1f42cb14999d6111c09f90cd
Crul <rauljavier.vila@gmail.com>
2016-07-25 19:27:53 +0200

    fix externalDbFactory export bug

--- meteor-external-mongo-db.js
+++ meteor-external-mongo-db.js
@@ -1,8 +1,8 @@
 'use strict'
 import ExternalDb from './lib/externalDb/externalDb';
-import ExternalDbFactory from './lib/externalDb/externalDbFactory';
+import externalDbFactory from './lib/externalDb/externalDbFactory';
 import ExternalDbPublisher from './lib/externalDb/externalDbPublisher';
 
 export const name = 'meteor-external-mongo-db';
-export { ExternalDbPublisher, ExternalDb, ExternalDbFactory };
+export { ExternalDbPublisher, ExternalDb, externalDbFactory };
 export default ExternalDbPublisher;
