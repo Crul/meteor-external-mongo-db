db462778e73339239a27b72a363cae233eebbfdb
Crul <rauljavier.vila@gmail.com>
2016-07-25 19:09:19 +0200

    v0.3.2 - code refactor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'crul:meteor-external-mongo-db',
-  version: '0.3.1',
+  version: '0.3.2',
   summary: 'external mongo db connector for angular meteor apps',
   git: 'https://github.com/Crul/meteor-external-mongo-db',
   documentation: 'README.md'
