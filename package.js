d4c78966bab04c8cb15996d30e686e932b6882ef
Crul <rauljavier.vila@gmail.com>
2016-07-26 20:25:19 +0200

    publish main module to both server and client

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('underscore');
   api.use('random');
-  api.mainModule('meteor-external-mongo-db.js', 'server');
+  api.mainModule('meteor-external-mongo-db.js');
 });
 
 Package.onTest(function(api) {
